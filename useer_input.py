987d9c54641631c21766ce48f63d8a6e017e64f6
rooney otieno <rooneyouko16@students.uonbi.ac.ke>
2024-10-31 22:17:32 +0300

    .

---
+++ useer_input.py
@@ -0,0 +1,4 @@
+name = input('what is your name ?:')
+age = input('what is your age?;')
+location = input('where are you located now? ;')
+print(f'hello{name}, you are {age} years old and you live in {location}')
